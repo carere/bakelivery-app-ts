cb9c1925ffee2c4e544add75dae81db12cfab803
Kevin Abatan <abatan.k@gmail.com>
2022-05-14 03:45:42 +0200

    chore: initializing project
    Initialize front-end project with Vite, React, Typescript, Prettier, Eslint, Husky, Commitizen,
    Lint-staged

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable fp/no-unused-expression */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
